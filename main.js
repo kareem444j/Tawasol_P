b1a3795f6a03f74b5823c5460ec4114344718f64
kareem444j <87791675+kareem444j@users.noreply.github.com>
2021-10-06 17:07:22 +0200

    Tawasol_P
    personal blog

---
+++ main.js
@@ -0,0 +1,28 @@
+function but1() {
+    document.getElementById("pwp").innerHTML = "01023681084";
+
+}
+
+function a1a() {
+    document.getElementById("p2p").innerHTML = "شكرا لك :)";
+    document.getElementById("p2p").className = "p2p"
+    document.getElementById("a11a").className = "a4a";
+    document.getElementById("a22a").className = "a4a";
+}
+
+function a2a() {
+    document.getElementById("p2p").innerHTML = ":( اخبرنا لماذا لم يعجبك الموقع";
+    document.getElementById("p2p").className = "p3p";
+    document.getElementById("a11a").className = "a4a";
+    document.getElementById("a22a").className = "a4a";
+    document.getElementById("text11").className = "text1";
+    document.getElementById("but111").className = "but222";
+
+}
+
+function put111() {
+    document.getElementById("p111p").innerHTML = "..تم الإرسال بنجاح";
+    document.getElementById("text11").className = "text14";
+    document.getElementById("but111").className = "text14";
+
+}
